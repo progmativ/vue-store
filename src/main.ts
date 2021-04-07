e2e6e0a89e3c6f3db8217aecc2d6590dbe9b90ad
Grzegorz Dybowicz <grzegorz.dybowicz@gmail.com>
2021-04-07 21:14:22 +0200

    feature: add firebase & basic firebase configuration & add config to prettier

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,11 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import { firebaseConfig } from "../firebase-config";
 
+import firebaseapp from "firebase/app";
+
+firebaseapp.initializeApp(firebaseConfig);
+
+console.log("firebaseapp", firebaseapp);
 createApp(App).use(store).use(router).mount("#app");
